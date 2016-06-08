5be0e63a2dfb9d674172d8329890edfb121ffad7
sunjeet-khokhar <sunjeetkhokhar@sunjeets-MBP.fritz.box>
2016-06-08 19:13:37 +1200

    A very basic zombie BDD test

--- spec/lib/zombie_spec.rb
+++ spec/lib/zombie_spec.rb
@@ -4,6 +4,20 @@ require "zombie"
 #declare the zombie class
 describe Zombie do 
 	#your examples (tests) do here
-   it "is named Night Walker Big poppy"	
+	zombie = Zombie.new
+   it "is named Ash" do
+   
+   zombie.name.should == 'Ash'
+   end
+
+   it "explodes on fire" do
+   #	zombie = Zombie.new
+   	zombie.on_fire.should == "Explode!"
+   end
+
+   
+   #it "killed" do
+   	#pending
+   #end
 end
 
--- spec/zombie.rb
+++ spec/zombie.rb
@@ -1,3 +1,13 @@
 class Zombie
+	attr_accessor :name
+
+	def initialize
+		@name = 'Ash'
+	end
+
+	def on_fire
+		return "Explode!"
+	end
+	
 	
 end
